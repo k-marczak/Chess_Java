51aa90b1097371956a70c20b78bacc7f2c12d3ac
k-marczak <k_marczak@outlook.com>
2023-08-08 19:50:56 +0200

    Add valid movements & collides with piece.

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -21,6 +21,55 @@ public class Rook extends Piece {
 
     }
 
+    public boolean isValidMovement(int col, int row) {
+
+        return this.col == col || this.row == row;
+
+    }
 
 
+    public boolean moveCollidesWithPiece(int col, int row) {
+
+        //left
+        if (this.col > col) {
+
+            for (int c = this.col - 1; c > col; c--) {
+                if (board.getPiece(c, this.row) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // right
+        if (this.col < col) {
+
+            for (int c = this.col + 1; c < col; c++) {
+                if (board.getPiece(c, this.row) != null) {
+                    return true;
+                }
+            }
+        }
+
+        //UP
+        if (this.row > row) {
+
+            for (int r = this.row - 1; r > row; r--) {
+                if (board.getPiece(this.col, r) != null) {
+                    return true;
+                }
+            }
+        }
+
+        // DOWN
+        if (this.row < row) {
+
+            for (int r = this.row + 1; r < row; r++) {
+                if (board.getPiece(this.col, r) != null) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
