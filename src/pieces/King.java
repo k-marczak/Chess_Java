85722eeefdf8a69664bcb42cba6e8642a8862962
k-marczak <k_marczak@outlook.com>
2023-08-10 18:06:46 +0200

    Problem fix

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -14,7 +14,7 @@ public class King extends Piece {
         this.xPos = col * board.tileSize;
         this.yPos = row * board.tileSize;
         this.isWhite = isWhite;
-        this.name = "pieces.King";
+        this.name = "King";
 
         this.sprite = sheet.getSubimage(0, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
     }
